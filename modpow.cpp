957c49f34b2cae6084ebca10c81044f0497cce7e
Shiv Satyam <84907785+demss233@users.noreply.github.com>
2023-08-05 22:56:12 +0530

    Update short_modular.cpp

--- modpow.cpp
+++ modpow.cpp
@@ -1,11 +1,20 @@
-int modpow(int a, int b, int m) {
-    a %= m;
-    int r = 1;
-    while (b > 0) {
-        if (b & 1)
-            r = r * a % m;
-        a = a * a % m;
-        b >>= 1;
-    }
-    return r;
+const int M = 1e9 + 7;
+int sum(int a, int b) {
+    a += b;
+    return (a >= M ? a - M : a);
+}
+int diff(int a, int b) {
+    return sum(a, M - b);
+}
+int mult(int a, int b) {
+    return a * 1ll * b % M;
+}
+int modpow(int n, int k) {
+    if(k == 0)
+        return 1;
+ 
+    int pw = pow2(n, k / 2);
+    pw = mul(pw, pw);
+ 
+    return (k % 2 == 0 ? pw : mul(pw, n));
 }
