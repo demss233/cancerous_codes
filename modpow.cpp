ac7ee1a3eed9b67245b698f580758cea2d6f2642
Shiv Satyam <84907785+demss233@users.noreply.github.com>
2023-08-20 01:43:00 +0530

    Update modpow.cpp

--- modpow.cpp
+++ modpow.cpp
@@ -1,6 +1,8 @@
+const int mod = 1e9 + 7;
+
 int modpow(int b, int e) {
     int ans = 1;
-    for (; e; b = b * b % P, e /= 2)
-        if (e & 1) ans = ans * b % P;
+    for (; e; b = b * b % mod, e /= 2)
+        if (e & 1) ans = ans * b % mod;
     return ans;
 }
