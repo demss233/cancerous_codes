f07d9e0f32c8ca05cc6c4699be26454823b22fa9
Shiv Satyam <84907785+demss233@users.noreply.github.com>
2023-09-13 14:35:56 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,27 +1,29 @@
-#include<bits/stdc++.h>
-// #include <ext/pb_ds/tree_policy.hpp>
-// #include <ext/pb_ds/assoc_container.hpp>
+#include <bits/stdc++.h>
 using namespace std;
-// using namespace __gnu_pbds;
-#define int long long
 #define ar array
-#define ld long double  
-#define f first
-#define sc second
-#define endl '\n'
+#define ld long double
+#define int long long
+#define sz(x) (int)x.size()
 #define all(x) x.begin(), x.end()
-#define lcm(x, y)(x * y) / (__gcd(x, y))
-#define sz(t)(int)(t.size())
-template <typename T> using min_heap = priority_queue<T, vector<T>, greater<T>>;
-#define debug(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+#define trace(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+#define fi first
+#define se second
+#define forn(i, j, n) for(int i = j; i < n; ++i)
+#define forn_(i, j, n) for(int i = j; i >= n; --i)
 void err(istream_iterator<string> it) {}
 template<typename T, typename... Args>
-void err(istream_iterator<string> it, T a, Args... args) {
+void err(istream_iterator<string> it, T a,Args... args) {
      cerr << *it << " = " << a << endl;
      err(++it, args...);
 }
-//template <class T> using tree = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
 
 int32_t main() {
-     cin.tie(0)->sync_with_stdio(0);
+     cin.tie(0)->sync_with_stdio(0); cin.exceptions(cin.failbit);
+
+     int t = 1;
+     // cin >> t;
+     while(t--) {
+          solve();
+     }
 }
+//txdy
